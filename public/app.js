04443762df2c27f0024f548100f71eabfe49fc20
nicklas-holmqvist <nicklas_holmqvist@outlook.com>
2021-04-13 14:50:01 +0200

    added list and buttons on html-page

--- public/app.js
+++ public/app.js
@@ -12,6 +12,9 @@ const formToDate = document.querySelector('#toDate')
 const myForm = document.querySelector('#myForm');
 const submit = document.querySelector('.submit');
 
+
+
+
 submit.addEventListener('click', submits)
 
 function showData() {
@@ -28,17 +31,80 @@ function showData() {
     }).catch(function (err) {
         console.log(err)
     })
-
 }
 
+
+
+// Append homeworks to html-page
 function viewHomeworks(homeworksData) {
+    const list = document.querySelector('#dataList')
     const homework = homeworksData
-    console.log(homework)
+
     for (let i = 0; i <homework.length; i++) {
-        console.log(homework[i].pages)
-        
+        const listItem = homework[i]
+
+        const listContainer = document.createElement('div');
+        listContainer.classList.add('listStyling', 'flex', 'row', 'center');
+        list.appendChild(listContainer)
+
+        // Datafield Subject
+        const subject = document.createElement("P");
+        subject.classList.add('textLeft')
+        subject.innerText = listItem.subject
+        listContainer.appendChild(subject)
+
+        // Datafield Pages
+        const pages = document.createElement("P");
+        pages.innerText = listItem.pages
+        listContainer.appendChild(pages)
+
+        // Datafield toDate
+        const toDate = document.createElement("P");
+        toDate.classList.add('textRight')
+        toDate.innerText = listItem.toDate
+        listContainer.appendChild(toDate)
+
+        // Button Delete
+        const btnDelete = document.createElement('button');
+        btnDelete.classList.add('btn', 'delete');
+        btnDelete.innerText = 'Ta bort';
+        listContainer.appendChild(btnDelete)
+
+        // Button Update
+        const btnUpdate = document.createElement('button');
+        btnUpdate.classList.add('btn', 'update');
+        btnUpdate.innerText = 'Uppdatera';
+        listContainer.appendChild(btnUpdate)     
+
+        btnDelete.addEventListener('click', (e) => {
+            console.log(listItem.id)
+            console.log(e)
+        })
+
+        btnUpdate.addEventListener('click', (e) => {
+            console.log(listItem.id)
+            console.log(e)
+        })
     }
+    
 }
+
+
+
+
+// const updateBtn = document.querySelector('.update')
+// updateBtn.addEventListener('click', (e) => {
+//     console.log(e)
+// })
+
+
+
+
+
+
+
+
+
 
 function submits(e) {
     e.preventDefault();
